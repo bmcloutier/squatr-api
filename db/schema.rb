18cf6b2c51b8e4d00a10b615ad500ba4fff08f27
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-08-06 11:39:43 -0500

    roomImages

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_05_192509) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_163620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "room_images", force: :cascade do |t|
+    t.integer "room_id"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id"
