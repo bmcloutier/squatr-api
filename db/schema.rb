409aac425331fdfb77ca4ed625f45783182751fe
NRosario21 <nosrea@gmail.com>
2024-08-06 11:53:05 -0500

    added Review Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_164544) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_165151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_164544) do
     t.string "end_date"
     t.decimal "price", precision: 10, scale: 2
     t.decimal "total", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "reservation_id"
+    t.integer "rating"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
