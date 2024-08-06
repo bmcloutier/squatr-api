b7b842413b9058ed8de0b21cd03b101b08ad441b
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-08-06 13:40:16 -0500

    added index route and jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.create(name: "Michael Jordan", email: "test@example.com", password: "password", password_confirmation: "password", image: "test.jpg")
+
+user.save
+
+room = Room.create(user_id: 1, address: "2700 Lincoln Ln", city: "Highland Park", state: "Illinois", price: 287.53, description: "this is MJs house", home_type: "mansion", room_type: "entire place", total_occupancy: 9, total_bedrooms: 9, total_bathrooms: 19)
+
+room.save
