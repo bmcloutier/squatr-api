d358db2e63df8820835a5556b6d9802f2491753a
Brian Cloutier <bmcloutier@gmail.com>
2024-08-08 13:26:41 -0500

    starter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,11 +17,7 @@ class UsersController < ApplicationController
       password_confirmation: params[:password_confirmation],
       image: params[:image],
     )
-    if user.save
-      render json: { message: "User created successfully" }, status: created
-    else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
-    end
+    render :show
   end
 
   def update
