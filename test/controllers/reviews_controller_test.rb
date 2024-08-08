555b3f4dc0c5009097812cdb22f239f3acab82f9
jefferymichalski <jeffery.michalskiusmc@gmail.com>
2024-08-08 12:46:34 -0500

    fixed remaining tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -6,26 +6,25 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get reviews_url
+    get reviews_path
     assert_response :success
   end
 
   test "should show review" do
-    pp reviews_url(@review)
-    get reviews_url(@review)
+    get reviews_path(@review)
     assert_response :success
   end
 
   test "should create review" do
     assert_difference("Review.count") do
-      post reviews_url, params: { review: { reservation_id: reservations(:one).id, rating: 5, comment: "Great stay!" } }
+      post reviews_path, params: { reservation_id: reservations(:one).id, rating: 5, comment: "Great stay!" }
     end
 
     assert_response :success
   end
 
   test "should update review" do
-    patch reviews_url(@review), params: { review: { comment: "Updated comment" } }
+    patch "/reviews/#{@review.id}.json", params: { comment: "Updated comment" }
     assert_response :success
     @review.reload
     assert_equal "Updated comment", @review.comment
@@ -33,7 +32,7 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
 
   test "should destroy review" do
     assert_difference("Review.count", -1) do
-      delete reviews_url(@review)
+      delete "/reviews/#{@review.id}.json"
     end
 
     assert_response :success
